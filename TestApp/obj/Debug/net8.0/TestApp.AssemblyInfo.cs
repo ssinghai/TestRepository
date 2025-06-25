47f17d7dcc2efa83ae8f403b92c77c8d29519b6f
Saurabh Singhai <ssinghai@cybage.com>
2025-06-25 16:14:49 +0530

    Add greeting and test message to Program.cs

--- TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433ab66e632673681ff92a1fe0d42b25f8511dfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
