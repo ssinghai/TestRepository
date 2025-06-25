05e1adc996c80db638b5d3d29af2e9082dc51ca9
Saurabh Singhai <ssinghai@cybage.com>
2025-06-25 16:53:33 +0530

    Implement salary calculation function and update Program.cs with example usage

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -2,3 +2,16 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("This is a test application");
+
+// Function to calculate salary based on base salary only
+static decimal CalculateSalary(decimal baseSalary)
+{
+    decimal hra = baseSalary * 0.20m;
+    decimal lta = baseSalary * 0.15m;
+    decimal bonus = baseSalary * 0.10m;
+    decimal otherComponents = baseSalary * 0.25m;
+    return baseSalary + hra + lta + bonus + otherComponents;
+}
+
+// Example usage
+Console.WriteLine($"Calculated Salary: {CalculateSalary(5000)}");
--- TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433ab66e632673681ff92a1fe0d42b25f8511dfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f17d7dcc2efa83ae8f403b92c77c8d29519b6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
